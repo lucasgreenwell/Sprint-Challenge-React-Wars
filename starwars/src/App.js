b87d5d0830fd72f111cf03e635715ec4a306c9d1
Lucas Greenwell <lucasgreenwell15@gmail.com>
2019-12-13 12:39:23 -0600

    about to try and stretch want this to not break

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import Person from "./components/person"
+import Person from "./components/person";
+import Buttons from "./components/buttons"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const Header = styled.h1`
+    text-align: center;
+    color: #443e3e;
+    text-shadow: 1px 1px 5px #fff;
+  `;
+
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
-      <Person num='1'/>
+      <Header>React Wars</Header>
+      <Person />
+      <Buttons/>
     </div>
   );
 };
---
+++ starwars/src/components/buttons.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Button } from 'reactstrap';
+import styled from "styled-components"
+
+const Buttons = () => {
+
+    const Parent = styled.div`
+        display: flex;
+        justify-content: space-around;
+    `;
+
+    return (<Parent>
+        <Button onClick={e => {
+            console.log('clicked');
+        }}>Previous</Button>
+            <Button>Next</Button>
+    </Parent>)
+}
+
+export default Buttons
